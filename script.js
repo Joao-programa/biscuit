f172364ecb7b303c1ac8bd71cbbdf223a7c67338
joao vieira <38202069-joaoprograma10@users.noreply.replit.com>
2024-12-05 13:36:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simulação de interação para "comprar" produtos
+document.querySelectorAll('button').forEach(button => {
+  button.addEventListener('click', () => {
+      alert('Produto adicionado ao carrinho!');
+  });
+});
